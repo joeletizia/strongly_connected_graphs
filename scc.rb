474095a81f7c57bcd07c0f6f02b6decfee3fd07f
Joe Letizia <joe@gust.com>
2015-02-20 10:08:33 -0500

    it works, but it blows up the ruby stack...fuuuuuuuu

--- scc.rb
+++ scc.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-lines = File.open("./small_test.txt", "r").readlines
+lines = File.open("./SCC.txt", "r").readlines
 
 graph = {}
 rev_graph = {}
@@ -13,7 +13,6 @@ lines.each do |line|
   rev_graph[split_line[1]] ||= []
   rev_graph[split_line[1]] << split_line[0]
 end
-
 
 def dfs_for_finishing_times(graph)
   finishing_times = {}
@@ -30,8 +29,10 @@ end
 def dfs(graph, start, explored_nodes, finishing_times)
   explored_nodes << start
 
-  graph[start].each do |child|
-    dfs(graph, child, explored_nodes, finishing_times) unless explored_nodes.include?(child)
+  unless graph[start].nil?
+    graph[start].each do |child|
+      dfs(graph, child, explored_nodes, finishing_times) unless explored_nodes.include?(child)
+    end
   end
 
   $t += 1
@@ -44,7 +45,6 @@ def dfs_for_leaders(graph, order)
 
   s = nil
 
-  binding.pry
   order.each do |item|
     s = item
     dfs_set_leaders(graph, item, leaders, explored_nodes, s) unless explored_nodes.include?(item)
@@ -57,19 +57,29 @@ def dfs_set_leaders(graph, item, leaders, explored_nodes, s)
   explored_nodes << item
   leaders[item] = s
 
-  graph[item].each do |child|
-    dfs_set_leaders(graph, child, leaders, explored_nodes, s) unless explored_nodes.include?(child)
+  unless graph[item].nil?
+    graph[item].each do |child|
+      dfs_set_leaders(graph, child, leaders, explored_nodes, s) unless explored_nodes.include?(child)
+    end
   end
 end
 
-binding.pry
 
 finishing_times = dfs_for_finishing_times(rev_graph)
 
 order_to_process_for_leaders = finishing_times.sort{|a,b| b[1] <=> a[1]}.map{|pair| pair[0]}
 
-binding.pry
 
 leader_hash = dfs_for_leaders(graph, order_to_process_for_leaders)
+
+leader_collection = []
+leader_hash.each do |key, value|
+  leader_collection[value] ||= []
+  leader_collection[value] << key
+end
+
+ordered_leaders = leader_collection.map{|item| item.nil? ? 0 : item.length }.sort{|a,b| b<=> a}.first(5)
+
+puts ordered_leaders.to_s
 
 binding.pry
